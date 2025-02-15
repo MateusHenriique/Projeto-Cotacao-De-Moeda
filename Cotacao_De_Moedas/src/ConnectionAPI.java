2c7ac56100978498b1a7a6eb29c089d12d0b9bfc
MateusHenriique <mateush5453@gmail.com>
2025-02-14 21:29:51 -0300

    comunicação com a api finalizada co ajustes

--- Cotacao_De_Moedas/src/ConnectionAPI.java
+++ Cotacao_De_Moedas/src/ConnectionAPI.java
@@ -1,15 +1,28 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ConnectionAPI {
-	
+
 	private String address = "https://economia.awesomeapi.com.br/json/last/";
-	
+
 	public String requestAPI(String requestCoin) {
 		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest.newBuilder()
-				.uri(URI.create(address + requestCoin + "-BRL"))
-				.build();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(address + requestCoin + "-BRL")).build();
+
+		HttpResponse<String> response = null;
+
+		try {
+			response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		return response.body();
+
 	}
 }
